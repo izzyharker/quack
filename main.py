6eb06ddb87e71d5d27d5b738e1fca023da667a85
Izzy Harker <harkerisabella@gmail.com>
2024-04-22 21:53:50 -0700

    updated parsing

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import os
 ASM_FILE = "test.asm"
 
 # nodes types for Nano-quack
-STRING, INT, OBJ, BOOLEAN, NOTHING = range(5)
+OBJ, INT, NEGINT, STRING, BOOL, NOTHING = range(6)
+
+node_types = {OBJ: "OBJ", INT: "INT", NEGINT: "NEGINT", STRING: "STRING", BOOL: "BOOL", NOTHING: "NOTHING"}
 
 class Obj():
     def __init__(self, value: None):
@@ -15,45 +17,61 @@ class Obj():
         return self.type
 
     def evaluate(self):
-        return self
+        # with open(ASM_FILE, "a") as f:
+        #     print(f"\tconst {self.val}", file=f)
+        # f.close()
+        print(f"\tconst {self.val}")
+    
+    def __str__(self):
+        return f"({node_types[self.type]}, {self.val})"
 
 class Int(Obj):
     def __init__(self, value: int):
+        super().__init__(value)
         self.val = value
+        self.type = INT
+
+class NegInt(Obj):
+    def __init__(self, value: int):
+        super().__init__(value)
+        self.val = abs(value)
         self.type = INT
 
 class String(Obj):
     def __init__(self, value: str):
+        super().__init__(value)
         self.val = value
         self.type = STRING
 
 class Bool(Obj):
     def __init__(self, value: bool):
+        super().__init__(value)
         self.val = value
-        self.type = BOOLEAN
+        self.type = BOOL
 
 class Nothing(Obj):
     def __init__(self):
+        super().__init__(None)
         self.val = None
         self.type = NOTHING
 
 class Variable(Obj):
-    vars = {}
+    vars: dict[str: Obj] = {}
 
     def __init__(self, name: str, value: Obj):
         self.name = name
-        self.value = value
-        self.type = value.get_type()
+        self.val = value
+        self.type = OBJ
 
-        Variable.vars[self.name] = self.type
+        Variable.vars[self.name] = self
 
     def get_type(self) -> int:
         return self.type
     
     def evaluate(self) -> Obj:
-        return self.value
+        self.val.evaluate()
 
-class Operator():
+class Operator(Obj):
     # operator tree
     ops = {"+": "plus", "-": "minus", "*": "times", "/": "divide"}
 
@@ -61,16 +79,25 @@ class Operator():
         self.left = left
         self.right = right
         if op in "+-*/":
+            self.token = op
             self.op = Operator.ops[op]
         else:
             raise SyntaxError(f"op {op} does not exist in Quack!")
 
     def evaluate(self):
-        a = self.left.evaluate()
-        b = self.right.evaluate()
+        self.left.evaluate()
+        self.right.evaluate()
 
         # print to file
+        # with open(ASM_FILE, "a") as f:
+        #     print(f"\tcall Int:{Operator.ops[self.op]}", file=f)
+        # f.close()
+        print(f"\tcall Int:{Operator.ops[self.op]}")
+        
         pass
+
+    def __str__(self):
+        return f"({self.left} {self.token} {self.right})"
 
 """
 grammar
@@ -78,8 +105,8 @@ grammar
 assign = '
 start -> var: [type] = sum;
 
-sum -> prod + prod (add)
-sum -> prod - prod (minus)
+sum -> sum + prod (add)
+sum -> sum - prod (minus)
 
 prod -> prod * prod (times)
 prod -> prod / prod (divide)
@@ -87,32 +114,113 @@ prod -> num
 '
 """
 
-def parse(expr: str) -> Obj:
-    pass
+class Tree():
+    types = {"NegInt": r"-\d+", "Int": r"\d+", "String": r"\"\w+\"", "Bool": r"True|False", "LParen": r"\(.*\)?", "RParen": r"\(?.*\)"}
 
-def evaluate(typ: str, expr: str) -> Obj:
-    eval = parse(expr)
-    pass
+    def __init__(self, expr: list[str]):
+        self.index = 0
+        self.tokens = expr
+        self.num_tokens = len(self.tokens)
+
+    def error(self):
+        raise Exception("Invalid syntax")
+    
+    def __str__(self):
+        return self.tokens
+    
+    def eat(self, expect: str = r".*"):
+        if re.match(expect, self.tokens[self.index]):
+            self.index += 1
+        else:
+            self.error()
+
+    def F(self):
+        """F : -num | num | var | ( expr ) | str"""
+        token = self.tokens[self.index]
+        print("F: ", token, self.index)
+        if token in list(Variable.vars.keys()):
+            self.eat()
+            return Variable.vars[token]
+        elif re.match(Tree.types["Bool"], token):
+            self.eat(Tree.types["Bool"])
+            if token == "true":
+                return Bool(True)
+            return Bool(False)
+        elif re.match(Tree.types["NegInt"], token):
+            self.eat(Tree.types["NegInt"])
+            return NegInt(int(token))
+        elif re.match(Tree.types["Int"], token):
+            self.eat(Tree.types["Int"])
+            return Int(int(token))
+        # TODO: parentheses
+        # elif re.match(Tree.types["LParen"], token):
+        #     # self.tokens[self.index] = re.match(r"(.*)", token).group()
+        #     self.eat()
+        #     node = self.E()
+        #     self.eat()
+        #     return node
+        elif re.match(Tree.types["String"], token):
+            self.eat(Tree.types["String"])
+            print(token)
+            return String(token)
+        else:
+            return Nothing()
+
+    def T(self):
+        """T : T * F | T / F | F"""
+        node = self.F()
+
+        while self.index < self.num_tokens and self.tokens[self.index] in "*/":
+            token = self.tokens[self.index]
+            if token == "*" or token == "/":
+                self.eat()
+
+            node = Operator(node, self.F(), token)
+        
+        print("T: ", node)
+        return node
+
+    def E(self):
+        """ 
+        E : E + T | E - T | T
+        """
+        node = self.T()
+
+        while self.index < self.num_tokens and self.tokens[self.index] in "+-":
+            token = self.tokens[self.index]
+            if token == "+" or token == "-":
+                self.eat()
+
+            node = Operator(node, self.T(), token)
+
+        print("E: ", node)
+        return node
+    
+    def parse(self):
+        return self.E()
+            
+
+
 
 def main():
-    try:
-        open(ASM_FILE, "x")
-    except FileExistsError:
-        os.remove(ASM_FILE)
+    # try:
+    #     open(ASM_FILE, "x")
+    # except FileExistsError:
+    #     os.remove(ASM_FILE)
 
-    # write header information
-    f = open(ASM_FILE, "a")
-    print(".class Calculator:Obj", file=f)
-    print(".method $constructor", file=f)
-    print("\tenter", file=f)
+    # # write header information
+    # f = open(ASM_FILE, "a")
+    # print(".class Calculator:Obj", file=f)
+    # print(".method $constructor", file=f)
+    # print("\tenter", file=f)
 
-    f.close()
+    # f.close()
 
     quack = []
 
     assignment = r"\w+:"
     var_type = r"Int|Bool|String|Obj"
-    expr = r"\= \-?.+([-+*/]\-?.+)*;"
+    arith_expr = r"\= \-?.+([-+*/]\-?.+)*;"
 
     # read quack
     with open("ex.qk", "r") as qk:
@@ -123,19 +231,26 @@ def main():
             temp = []
             temp.append(re.match(assignment, line).group().strip(":"))
             temp.append(re.search(var_type, line).group())
-            temp.append(re.search(expr, line).group().strip("=;"))
+            temp.append(re.search(arith_expr, line).group().strip("=;"))
             quack.append(temp)
 
     # build tree
-    # print(quack)
+    print(quack)
 
     for line in quack:
-        expr = evaluate(line[1], line[2])
-        var = Variable(line[0], expr)
+        expr = line[2].split()
+        print(expr)
+        eval = Tree(expr)
+        eval = eval.parse()
+        print(eval)
+        var = Variable(line[0], eval)
+        print(var.name)
+        print(var.val)
+        # what to do with the variable...
 
-    f = open(ASM_FILE, "a")
-    print("\treturn 0", file=f)
-    f.close()
+    # f = open(ASM_FILE, "a")
+    # print("\treturn 0", file=f)
+    # f.close()
 
 if __name__ == "__main__":
     main()
